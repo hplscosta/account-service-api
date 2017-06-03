ba987dd797a12e0b8a01a542b9b9ce2906733607
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-03 18:49:51 +0100

    add insert account method

--- src/main/java/com/demo/account/persistence/api/account/AccountService.java
+++ src/main/java/com/demo/account/persistence/api/account/AccountService.java
@@ -1,6 +1,9 @@
 package com.demo.account.persistence.api.account;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 /**
  * Account service to validate accounts and interacts with persistence layer. <br>
@@ -9,7 +12,17 @@ import org.springframework.stereotype.Service;
  * @author Hugo Costa
  * @since 1.0.0
  */
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class AccountService {
 
+	private final AccountRepository repository;
+
+	public Account insert( Account account ) {
+		Assert.hasText( account.getUser(), "User cannot be empty." );
+		Assert.hasText( account.getName(), "Name cannot be empty." );
+		log.info( "Inserting user {} with name {}", account.getUser(), account.getName() );
+		return repository.insert( account );
+	}
 }
