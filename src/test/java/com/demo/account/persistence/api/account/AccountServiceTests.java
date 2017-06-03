2b856e83127caee6b7db4a82f969d83c7e44194d
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-03 18:12:05 +0100

    add account service test

---
+++ src/test/java/com/demo/account/persistence/api/account/AccountServiceTests.java
@@ -0,0 +1,13 @@
+package com.demo.account.persistence.api.account;
+
+/**
+ * Testing {@link AccountService} methods. <br>
+ * Mocks {@link AccountRepository}.
+ *
+ * @author Hugo Costa
+ * @since 1.0.0
+ */
+public class AccountServiceTests {
+
+
+}
