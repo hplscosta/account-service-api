1df558446ed13e4e4174f1db1ef679f00a3b0eee
hugo costa <hugo-costa>
2017-06-03 14:50:55 +0100

    create spring boot project with starters (web and mongo) and jetty server

---
+++ src/main/java/com/demo/account/persistence/api/AccountPersistenceApiApplication.java
@@ -0,0 +1,12 @@
+package com.demo.account.persistence.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountPersistenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountPersistenceApiApplication.class, args);
+	}
+}
