d0aeaa63d00a2ca34f3c73c8c57c06a8b05cfebd
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-03 17:35:04 +0100

    add mongo config tests to initialize a embedded mongo instance

---
+++ src/test/java/com/demo/account/persistence/api/config/MongoConfigTests.java
@@ -0,0 +1,19 @@
+package com.demo.account.persistence.api.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Import {@link MongoConfig} to enable Mongo repositories <br>
+ * Enable auto configuration to initialize a Embedded Mongo instance.
+ *
+ * @author Hugo Costa
+ * @since 1.0.0
+ */
+@Configuration
+@EnableAutoConfiguration
+@Import( MongoConfig.class )
+public class MongoConfigTests {
+
+}
