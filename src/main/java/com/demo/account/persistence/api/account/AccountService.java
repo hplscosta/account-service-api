b94aabf871a9ea08ac0c5628204b6f7f0fa6009e
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-03 18:12:59 +0100

    add account service to validate accounts and interacts with persistence layer

---
+++ src/main/java/com/demo/account/persistence/api/account/AccountService.java
@@ -0,0 +1,15 @@
+package com.demo.account.persistence.api.account;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Account service to validate accounts and interacts with persistence layer. <br>
+ * Interacts with {@link AccountRepository}.
+ *
+ * @author Hugo Costa
+ * @since 1.0.0
+ */
+@Service
+public class AccountService {
+
+}
