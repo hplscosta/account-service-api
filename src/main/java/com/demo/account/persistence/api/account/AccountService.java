7aac6dca52de0332012e608aa60f588d87fc90e5
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-03 19:09:11 +0100

    add new methods to account service: findOne and findAll

--- src/main/java/com/demo/account/persistence/api/account/AccountService.java
+++ src/main/java/com/demo/account/persistence/api/account/AccountService.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.util.List;
+
 /**
  * Account service to validate accounts and interacts with persistence layer. <br>
  * Interacts with {@link AccountRepository}.
@@ -19,10 +21,38 @@ public class AccountService {
 
 	private final AccountRepository repository;
 
+	/**
+	 * Validates mandatory properties and calls {@link AccountRepository} to save account.
+	 *
+	 * @param {@link Account}
+	 * @return a new {@link Account}
+	 */
 	public Account insert( Account account ) {
 		Assert.hasText( account.getUser(), "User cannot be empty." );
 		Assert.hasText( account.getName(), "Name cannot be empty." );
 		log.info( "Inserting user {} with name {}", account.getUser(), account.getName() );
 		return repository.insert( account );
+	}
+
+	/**
+	 * Calls {@link AccountRepository} to find {@link Account} by user.
+	 *
+	 * @param user
+	 * @return {@link Account}
+	 */
+	public Account find( String user ) {
+		Assert.hasText( user, "User cannot be empty." );
+		log.info( "Looking for user {}", user );
+		return repository.findOne( user );
+	}
+
+	/**
+	 * Calls {@link AccountRepository} to find all {@link Account} in repository.
+	 *
+	 * @return a list of {@link Account}
+	 */
+	public List<Account> findAll() {
+		log.info( "Getting all users" );
+		return repository.findAll();
 	}
 }
